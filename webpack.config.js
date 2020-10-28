d706f8035108bb89ba007fb5b01f84b4f1617a41
junishakhatri17 <junisha.khatri@gmail.com>
2020-10-28 19:07:05 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './server/index.js',
+
+  target: 'node',
+
+  externals: [nodeExternals()],
+
+  output: {
+    path: path.resolve('server-build'),
+    filename: 'index.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("node-sass"),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|j?g|svg|gif)?$/,
+        use: "file-loader",
+      },
+    ],
+  }
+};
