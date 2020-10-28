217779fdcdcd29ef81e418a28868702f46ad4dd3
junishakhatri17 <junisha.khatri@gmail.com>
2020-10-28 18:39:07 +0530

    Add files via upload

---
+++ build/precache-manifest.6498364fadb17a2a069584ffe3ba99e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df027f43a28c4ecef67ba6ba568ec15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ef27df1f58c9f8d9c8f",
+    "url": "/static/css/main.ff83461b.chunk.css"
+  },
+  {
+    "revision": "65a8e79cc082ef84666e",
+    "url": "/static/js/2.827b6cd4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.827b6cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef27df1f58c9f8d9c8f",
+    "url": "/static/js/main.2291e21e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "77c0bad9781ccc85c70d106c00c2acea",
+    "url": "/static/media/img1.1c8ab527.png"
+  }
+]);
